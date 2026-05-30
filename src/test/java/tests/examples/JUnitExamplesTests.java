7120ff6b8b91eca62f1f4ad5a70ecf0b271b8b1f
anna1-89 <anna1_89@mail.ru>
2026-05-30 20:05:28 +0300

    Tests examples

--- src/test/java/tests/examples/JUnitExamplesTests.java
+++ src/test/java/tests/examples/JUnitExamplesTests.java
@@ -1,29 +1,37 @@
 package tests.examples;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 public class JUnitExamplesTests {
 
-   @BeforeEach
+    @BeforeAll
+    static void startUp(){
+        System.out.println("tests startup");
+    }
+
+    @AfterAll
+    static void tearDown(){
+        System.out.println("tests ended");
+    }
+
+    @BeforeEach
     void startBrower(){
-       System.out.println("browser has started");
+       System.out.println("-  browser has started");
    }
 
     @AfterEach
     void stopBrower(){
-        System.out.println("browser has stopped");
+        System.out.println("-  browser has stopped");
     }
 
     @Test
     void firstTest(){
-        System.out.println("- firstTest");
+        System.out.println("-- firstTest");
     }
 
     @Test
     void secondTest(){
-        System.out.println("- secondTest");
+        System.out.println("-- secondTest");
 
     }
 
