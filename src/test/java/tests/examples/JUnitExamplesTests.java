ecb8d050c32660a05062b856c44696b9ff713eef
anna1-89 <anna1_89@mail.ru>
2026-05-30 19:58:08 +0300

    Add tests

--- src/test/java/tests/examples/JUnitExamplesTests.java
+++ src/test/java/tests/examples/JUnitExamplesTests.java
@@ -1,11 +1,30 @@
 package tests.examples;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class JUnitExamplesTests {
 
+   @BeforeEach
+    void startBrower(){
+       System.out.println("browser has started");
+   }
+
+    @AfterEach
+    void stopBrower(){
+        System.out.println("browser has stopped");
+    }
+
     @Test
     void firstTest(){
+        System.out.println("- firstTest");
+    }
+
+    @Test
+    void secondTest(){
+        System.out.println("- secondTest");
 
     }
+
 }
